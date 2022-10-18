2663503a9439c08cc541715669865c7f4ed37afa
LZY_Shrio <170280176@qq.com>
2022-10-18 18:17:37 +0800

    chore：project update

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -1,13 +1,45 @@
 import type { NextPage } from 'next';
+import * as React from 'react';
 import Layout from '@/components/layout/Layout';
-import Head from 'next/head';
-import styles from '../styles/Home.module.less';
+import Seo from '@/components/Seo';
+import classNames from 'classnames';
+import styles from '../styles/Project.module.scss';
+import UnstyledLink from 'link/UnstyledLink';
+import useLoaded from 'hooks/useLoaded';
 
 const Project: NextPage = () => {
+  const isLoaded = useLoaded();
   return (
-    <>
-      <Layout>project</Layout>
-    </>
+    <Layout>
+      <Seo
+        templateTitle="Blog"
+        description="Thoughts, mental models, and tutorials about front-end development. Rebuild your mental model so front-end development can be predictable."
+      />
+      <main>
+        <section
+          className={classNames(isLoaded && 'fade-in-start m-auto', styles.wrapper)}
+        >
+          <ul className="flex" data-fade="1">
+            <li className={styles.box}>
+              <div>
+                <span
+                  className="iconfont icon-Dmoxingx font-bold"
+                  style={{ fontSize: 38 }}
+                ></span>
+              </div>
+            </li>
+            <li className={styles.box}>
+              <div>
+                <span
+                  className="iconfont icon-Dmoxingx font-bold"
+                  style={{ fontSize: 38 }}
+                ></span>
+              </div>
+            </li>
+          </ul>
+        </section>
+      </main>
+    </Layout>
   );
 };
 
