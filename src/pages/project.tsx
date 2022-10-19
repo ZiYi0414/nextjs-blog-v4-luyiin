2c09f0bf8015e6d4e692c56a271c85d2a4d515ac
LZY_Shrio <170280176@qq.com>
2022-10-19 16:42:03 +0800

    feat(project)：i dont know！！！！

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -9,6 +9,47 @@ import useLoaded from 'hooks/useLoaded';
 
 const Project: NextPage = () => {
   const isLoaded = useLoaded();
+  const project = [
+    {
+      title: 'A 3D Character Powerby Lingo3D',
+      icon: (
+        <span
+          className="iconfont icon-Dmoxingx font-bold mr-4"
+          style={{ fontSize: 38 }}
+        ></span>
+      ),
+      desc: '瞎折腾，一个自己的3D形象.小萌新..',
+      tag: ['React', 'Lingo3D', 'Blender'],
+      github: '',
+      url: 'https://mine.nopatience.cn/'
+    },
+    {
+      title: '八卦',
+      desc: '想做，就做了。',
+      icon: (
+        <span
+          className="iconfont icon-bagua font-bold mr-4"
+          style={{ fontSize: 38 }}
+        ></span>
+      ),
+      tag: ['CSS', '周易'],
+      github: '',
+      url: ''
+    },
+    {
+      title: 'Handsome-CSS3-lib',
+      desc: 'Building....',
+      icon: (
+        <span
+          className="iconfont icon-css3 font-bold mr-4"
+          style={{ fontSize: 38 }}
+        ></span>
+      ),
+      tag: ['React', 'CSS', 'Ctrl + CV'],
+      github: '',
+      url: ''
+    }
+  ];
   return (
     <Layout>
       <Seo
@@ -17,25 +58,43 @@ const Project: NextPage = () => {
       />
       <main>
         <section
-          className={classNames(isLoaded && 'fade-in-start m-auto', styles.wrapper)}
+          className={classNames(
+            isLoaded && 'fade-in-start m-auto',
+            styles.wrapper
+          )}
         >
-          <ul className="flex" data-fade="1">
-            <li className={styles.box}>
-              <div>
-                <span
-                  className="iconfont icon-Dmoxingx font-bold"
-                  style={{ fontSize: 38 }}
-                ></span>
-              </div>
-            </li>
-            <li className={styles.box}>
-              <div>
-                <span
-                  className="iconfont icon-Dmoxingx font-bold"
-                  style={{ fontSize: 38 }}
-                ></span>
-              </div>
-            </li>
+          <div className={classNames(styles.title, 'my-4')}>
+            <div className="text-[2rem] mb:text-[1.5rem]">
+              一些不怎么样的 <span className="text-[#d23669]">臆想</span>？
+            </div>
+            <p className="italic text-gray-600">
+              I wish the world would be peaceful.
+            </p>
+          </div>
+          <ul className="flex flex-wrap" data-fade="1">
+            {project.map((e, idx) => (
+              <li className={styles.box} key={e.title + idx}>
+                <UnstyledLink href={e?.url}>
+                  <div className="flex items-center">
+                    {e?.icon}
+                    <div>
+                      <span className="truncate-one">{e?.title}</span>
+                      <small>{e?.desc}</small>
+                      <div className="flex mt-1">
+                        {e.tag.map((tag, index) => (
+                          <span
+                            className="text-xs border border-current rounded px-1 pb-0.2 mr-2 "
+                            key={tag + index}
+                          >
+                            {tag}
+                          </span>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                </UnstyledLink>
+              </li>
+            ))}
           </ul>
         </section>
       </main>
