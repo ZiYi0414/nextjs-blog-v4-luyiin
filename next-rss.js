e1aca6b58931374ca268c8066e741f78373ab04f
LZY_Shrio <170280176@qq.com>
2022-11-17 20:20:57 +0800

    feat：xiba loma 😂

---
+++ next-rss.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteTitle: 'Luyiin Blog',
+  siteDescription: 'Luyiin Blog rss feed',
+  siteLanguage: 'zh',
+  siteCopyright: '©Luyiin',
+  siteUrl: 'https://luyiin.me',
+  outDir: 'public',
+  postsDir: 'contents/blog',
+}
